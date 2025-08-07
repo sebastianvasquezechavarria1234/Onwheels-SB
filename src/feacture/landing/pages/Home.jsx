e7d26b5ecdcd76d2b91c65144a30e591c56b18a2
sebastianvasquezechavarria1234 <sebitas221314@gmail.com>
2025-08-07 16:28:57 -0500

    primary

---
+++ src/feacture/landing/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Home = () => {
+    return (
+        <section className="max-w-[1200px] mx-auto pt-[120px]">
+            <h1 className="max-w-[700px]">Home</h1>  
+
+        </section>
+    )
+}
