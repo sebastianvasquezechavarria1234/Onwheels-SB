b2a00f52f15e0f2d2532c79832c8afd7913b0825
bobadas1234 <anasofiagranda5@gmail.com>
2025-08-27 18:31:23 -0500

    s

--- src/feacture/landing/pages/Home.jsx
+++ src/feacture/landing/pages/Home.jsx
@@ -4,7 +4,7 @@ export const Home = () => {
     return (
         <section className="max-w-[1200px] mx-auto pt-[120px]">
             <h1 className="max-w-[700px]">Home</h1>  
-
+            p
         </section>
     )
 }
