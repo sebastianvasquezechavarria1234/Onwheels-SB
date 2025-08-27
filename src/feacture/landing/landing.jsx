46ac90a89428ff642b3f9fb4bc2f18303527d4a8
EmanuelGJ01 <emanuelgj666@gmail.com>
2025-08-27 18:23:18 -0500

    rr

---
+++ src/feacture/landing/landing.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const Landing =() =>{
+
+    return(
+        <section>
+            <div>
+
+            </div>
+
+        </section>
+
+        
+
+    )
+
+}
