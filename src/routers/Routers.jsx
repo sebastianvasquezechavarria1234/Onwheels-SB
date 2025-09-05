abea39177839bb4f7173a1a5b111fd09e52bf228
EmanuelGJ01 <emanuelgj666@gmail.com>
2025-09-05 09:21:45 -0500

    s

--- src/routers/Routers.jsx
+++ src/routers/Routers.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import { Home } from "../feacture/landing/pages/Home";
-import { Shop } from "../feacture/landing/pages/Store";
-import { Class } from "../feacture/landing/components/class/class";
-<<<<<<< HEAD
+import Home from "../feacture/landing/pages/Home";
+import Shop from "../feacture/landing/pages/Store";
+import Class from "../feacture/landing/components/class/class";
 import Preinscriptions from "../feacture/landing/pages/preinscriptions";
 import Events from "../feacture/landing/events/events";
 import Sidebar from "../feacture/dashboard/layout-dashboard/sidebar";
@@ -15,13 +14,8 @@ import Sedes from "../feacture/dashboard/eventos/sedes/sedes";
 import Patrocinadores from "../feacture/dashboard/eventos/patrocinadores/patrocinadores";
 import CategoriasEventos from "../feacture/dashboard/eventos/categoria-eventos/categoria-eventos";
 import Eventos from "../feacture/dashboard/eventos/eventos/eventos";
-=======
-import Preinscriptions from "../feacture/landing/components/class/preinscriptions/preinscriptions";
 import Login from "../feacture/landing/Auth/Login";
 import Register from "../feacture/landing/Auth/Register";
-
-
->>>>>>> eaeeb97541d26ceddf491c55793fe9acc27b7336
 
 export const Routers = () => {
     return(
@@ -29,15 +23,10 @@ export const Routers = () => {
             <Route path="/" element={<Home />}/>
             <Route path="shop" element={<Shop />}/>
             <Route path="class" element={<Class />}/>
-<<<<<<< HEAD
             <Route path="preinscriptions" element={<Preinscriptions />}/>
             <Route path="events" element={<Events />}/>
             <Route path="sidebar" element={<Sidebar />}/>
-
-            
             <Route path="dashboard" element={<Estudiantes />}/>
-
-
             <Route path="/dashboard/estudiantes" element={<Estudiantes />}/>
             <Route path="/dashboard/productos" element={<Productos />}/>
             <Route path="/dashboard/categoria-productos" element={<Categorias />}/>
@@ -46,16 +35,8 @@ export const Routers = () => {
             <Route path="/dashboard/patrocinadores" element={<Patrocinadores />}/> 
             <Route path="/dashboard/categorias-eventos" element={<CategoriasEventos/>}/> 
             <Route path="/dashboard/eventos" element={<Eventos/>}/> 
-            
-=======
-            <Route path="preinscriptions" element={<Preinscriptions/>}/>
             <Route path="login" element={<Login/>}/>
             <Route path="register" element={<Register/>}/>
-
-
-            
->>>>>>> eaeeb97541d26ceddf491c55793fe9acc27b7336
         </Routes>
-
     )
 }
